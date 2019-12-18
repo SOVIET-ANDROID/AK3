31db4865b3e0c1b08a5d42e25724bca066d4c951
kondors1995 <normandija1945@gmail.com>
2019-12-18 15:57:46 +0000

    WIP add utsav ak

--- anykernel.sh
+++ anykernel.sh
@@ -11,7 +11,7 @@ do.cleanup=1
 do.cleanuponabort=1
 device.name1=raphael
 device.name2=raphaelin
-supported.versions=raphael_eea
+supported.versions=
 supported.patchlevels=
 '; } # end properties
 
